14d4c996a614d906db5bdc70c4ffa915ed86cdf0
Felix Schlitter <felixschlitter@gmail.com>
2016-04-17 07:37:41 +1200

    Add hacky webpack workaround for linked module

--- src/containers/Playground.js
+++ src/containers/Playground.js
@@ -1,7 +1,8 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { setSource, setArgv } from 'redux/modules/neodoc';
+import { setSource, setArgv, setOptionsFirst } from 'redux/modules/neodoc';
 import Codemirror from 'react-codemirror';
 
 type Props = {
@@ -16,19 +17,34 @@ export class Playground extends React.Component {
         <div>
           <h4>Write your help text</h4>
           <Codemirror
+            ref="editor"
             value={this.props.source}
             onChange={this.props.setSource}
           />
         </div>
         <div>
-          <h4>ARGV:</h4>
-          <input type="text" value={this.props.argv} onChange={this.props.setArgv}/>
+          <h4>Options:</h4>
+
+          <input name="options-first"
+                 type="checkbox"
+                 checked={this.props.optionsFirst}
+                 defaultChecked={this.props.optionsFirst}
+                 onChange={this.props.setOptionsFirst}/>
+          <label for="options-first">Enable options-first</label>
+
+          <h4>Command line:</h4>
+          <input type="text"
+                 value={this.props.argv}
+                 onChange={this.props.setArgv}/>
         </div>
         <div>
         {
           (this.props.error)
             ? (<div class="error" style={{whiteSpace: 'pre'}}>
-                { this.props.error.message }
+                <pre>
+                  { this.props.error.message }
+                </pre>
+
               </div>)
             : (<div class="output">
                 { JSON.stringify(this.props.output) }
@@ -47,10 +63,11 @@ const mapStateToProps = (state) => ({
   error:  state.neodoc.error
 })
 
-const mapDispatchToProps = {
-  setSource,
-  setArgv: (event) => setArgv(event.target.value)
-}
+const mapDispatchToProps = (dispatch) => ({
+  setSource:       _.debounce(s => dispatch(setSource(s)), 100),
+  setArgv:         (event) => dispatch(setArgv(event.target.value)),
+  setOptionsFirst: (event) => dispatch(setOptionsFirst(event.target.checked))
+})
 
 export default connect(
   mapStateToProps,
