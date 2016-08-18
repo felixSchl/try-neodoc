e369ac10ba252e9f6dc7c08ecdefb7c7da6709a2
Felix Schlitter <felixschlitter@gmail.com>
2016-08-18 15:45:23 +1200

    Update version

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -25,7 +25,7 @@ export class HomeView extends React.Component {
           <div id='topsection'>
             <h1 className='title'>&lt;neodoc&gt;</h1>
             <h5 className='subtitle'>
-              Beautiful, handcrafted commandlines <sub>v0.10.0</sub>
+              Beautiful, handcrafted commandlines <sub>v0.10.1</sub>
             </h5>
             <div className='github'>
               <ul>
